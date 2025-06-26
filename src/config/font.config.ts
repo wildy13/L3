5f9ed5223dd9d788cb6b9060169b03eaf5abff18
wildy13 <wildy.simanjuntak13@gmail.com>
2025-06-26 10:14:38 +0700

    updated

--- src/config/font.config.ts
+++ src/config/font.config.ts
@@ -31,25 +31,25 @@ function generateInterVariants(basePath: URL): FontVariant[] {
 
 export const fontRegistry: FontRegistry = {
     'space-grotesk': [
-        { weight: 'regular', style: 'normal', path: new URL('../../../dist/assets/fonts/Space_Grotesk/static/SpaceGrotesk-Regular.ttf', import.meta.url).href },
-        { weight: 'light', style: 'normal', path: new URL('../../../dist/assets/fonts/Space_Grotesk/static/SpaceGrotesk-Light.ttf', import.meta.url).href },
-        { weight: 'bold', style: 'normal', path: new URL('../../../dist/assets/fonts/Space_Grotesk/static/SpaceGrotesk-Bold.ttf', import.meta.url).href },
-        { weight: 'medium', style: 'normal', path: new URL('../../../dist/assets/fonts/Space_Grotesk/static/SpaceGrotesk-Medium.ttf', import.meta.url).href },
-        { weight: 'semibold', style: 'normal', path: new URL('../../../dist/assets/fonts/Space_Grotesk/static/SpaceGrotesk-SemiBold.ttf', import.meta.url).href },
+        { weight: 'regular', style: 'normal', path: new URL('./assets/fonts/Space_Grotesk/static/SpaceGrotesk-Regular.ttf', import.meta.url).href },
+        { weight: 'light', style: 'normal', path: new URL('./assets/fonts/Space_Grotesk/static/SpaceGrotesk-Light.ttf', import.meta.url).href },
+        { weight: 'bold', style: 'normal', path: new URL('./assets/fonts/Space_Grotesk/static/SpaceGrotesk-Bold.ttf', import.meta.url).href },
+        { weight: 'medium', style: 'normal', path: new URL('./assets/fonts/Space_Grotesk/static/SpaceGrotesk-Medium.ttf', import.meta.url).href },
+        { weight: 'semibold', style: 'normal', path: new URL('./assets/fonts/Space_Grotesk/static/SpaceGrotesk-SemiBold.ttf', import.meta.url).href },
     ],
-    'inter-18pt': generateInterVariants(new URL('../../../dist/assets/fonts/Inter/static/Inter_18pt-', import.meta.url)),
-    'inter-24pt': generateInterVariants(new URL('../../../dist/assets/fonts/Inter/static/Inter_24pt-', import.meta.url)),
-    'inter-28pt': generateInterVariants(new URL('../../../dist/assets/fonts/Inter/static/Inter_28pt-', import.meta.url)),
+    'inter-18pt': generateInterVariants(new URL('./assets/fonts/Inter/static/Inter_18pt-', import.meta.url)),
+    'inter-24pt': generateInterVariants(new URL('./assets/fonts/Inter/static/Inter_24pt-', import.meta.url)),
+    'inter-28pt': generateInterVariants(new URL('./assets/fonts/Inter/static/Inter_28pt-', import.meta.url)),
     'roboto': [
-        { weight: 'regular', style: 'normal', path: new URL('../../../dist/assets/fonts/Roboto/static/Roboto-Regular.ttf', import.meta.url).href },
-        { weight: 'bold', style: 'normal', path: new URL('../../../dist/assets/fonts/Roboto/static/Roboto-Bold.ttf', import.meta.url).href },
-        { weight: 'light', style: 'italic', path: new URL('../../../dist/assets/fonts/Roboto/static/Roboto-LightItalic.ttf', import.meta.url).href },
+        { weight: 'regular', style: 'normal', path: new URL('./assets/fonts/Roboto/static/Roboto-Regular.ttf', import.meta.url).href },
+        { weight: 'bold', style: 'normal', path: new URL('./assets/fonts/Roboto/static/Roboto-Bold.ttf', import.meta.url).href },
+        { weight: 'light', style: 'italic', path: new URL('./assets/fonts/Roboto/static/Roboto-LightItalic.ttf', import.meta.url).href },
     ],
     'roboto-condensed': [
-        { weight: 'regular', style: 'normal', path: new URL('../../../dist/assets/fonts/Roboto/static/Roboto_Condensed-Regular.ttf', import.meta.url).href },
-        { weight: 'bold', style: 'italic', path: new URL('../../../dist/assets/fonts/Roboto/static/Roboto_Condensed-BoldItalic.ttf', import.meta.url).href },
+        { weight: 'regular', style: 'normal', path: new URL('./assets/fonts/Roboto/static/Roboto_Condensed-Regular.ttf', import.meta.url).href },
+        { weight: 'bold', style: 'italic', path: new URL('./assets/fonts/Roboto/static/Roboto_Condensed-BoldItalic.ttf', import.meta.url).href },
     ],
     'roboto-semicondensed': [
-        { weight: 'regular', style: 'normal', path: new URL('../../../dist/assets/fonts/Roboto/static/Roboto_SemiCondensed-Regular.ttf', import.meta.url).href },
+        { weight: 'regular', style: 'normal', path: new URL('./assets/fonts/Roboto/static/Roboto_SemiCondensed-Regular.ttf', import.meta.url).href },
     ],
 };
