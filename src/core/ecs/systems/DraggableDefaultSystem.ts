81e86317ed8c51e3eb27d2d2bdd613c61f7b75ed
wildy13 <wildy.simanjuntak13@gmail.com>
2025-07-09 13:32:58 +0700

    add new system

---
+++ src/core/ecs/systems/DraggableDefaultSystem.ts
@@ -0,0 +1,45 @@
+import { Attributes, System } from 'ecsy';
+import { Object3DComponent } from '../components/Object3DComponent';
+import { MathUtils, Quaternion, Vector3 } from 'three';
+import { DraggableDefaultComponent } from '../components/DraggableDefaultComponent';
+
+export class DraggableDefaultSystem extends System {
+    init(attributes?: Attributes): void { }
+
+    execute(delta: number, time: number): void {
+        this.queries.draggableReturn.results.forEach(entity => {
+            const draggable = entity.getMutableComponent(DraggableDefaultComponent);
+            const object = entity.getComponent(Object3DComponent)?.object;
+
+            if (draggable?.originalParent === null) draggable.originalParent = object?.parent;
+
+            switch (draggable?.state) {
+                case 'to-be-attached':
+                    draggable?.attachedPointer.attach(object);
+                    draggable.state = 'attached';
+                    break;
+                case 'to-be-detached':
+                    draggable.originalParent.attach(object);
+                    draggable.state = 'detached';
+                    break;
+                case 'to-be-draggable':
+                    if (!object?.position) break;
+
+                    object.position.z = MathUtils.damp(object?.position.z, -0.3, 0.1, 0.6);
+                    if (Math.abs(object.position.z - 0.1) < 0.01) {
+                        draggable.state = 'attached';
+                    }
+                    break;
+                default:
+                    object?.scale.set(1, 1, 1);
+            }
+        });
+    }
+
+}
+
+DraggableDefaultSystem.queries = {
+    draggableReturn: {
+        components: [DraggableDefaultComponent]
+    }
+};
