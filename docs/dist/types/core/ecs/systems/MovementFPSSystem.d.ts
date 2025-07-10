82ffb3ab669615f3934b9b40a4c14efe1756cd40
wildy13 <wildy.simanjuntak13@gmail.com>
2025-07-10 15:02:54 +0700

    update dist

--- docs/dist/types/core/ecs/systems/MovementFPSSystem.d.ts
+++
@@ -1,7 +0,0 @@
-import { Attributes, System } from "ecsy";
-import { Vector3 } from "three";
-export declare class MovementFPSSystem extends System {
-    movement: Vector3;
-    init(attributes?: Attributes): void;
-    execute(delta: number, time: number): void;
-}
