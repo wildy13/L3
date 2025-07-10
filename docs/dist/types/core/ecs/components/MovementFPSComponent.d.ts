aa70f311dd2af75dc5590e69bdc4a554376055f0
wildy13 <wildy.simanjuntak13@gmail.com>
2025-07-10 14:10:24 +0700

    update component and dist

--- docs/dist/types/core/ecs/components/MovementFPSComponent.d.ts
+++ docs/dist/types/core/ecs/components/MovementFPSComponent.d.ts
@@ -2,9 +2,4 @@ import { Group } from 'three';
 import { Component } from 'ecsy';
 export declare class MovementFPSComponent extends Component<MovementFPSComponent> {
     player: Group | any;
-    axesX: number;
-    axesZ: number;
-    isWalking: boolean;
-    isRotating: boolean;
-    speed: number;
 }
--- src/core/ecs/components/MovementFPSComponent.ts
+++ src/core/ecs/components/MovementFPSComponent.ts
@@ -3,20 +3,8 @@ import { Component, Types } from 'ecsy';
 
 export class MovementFPSComponent extends Component<MovementFPSComponent> {
     player: Group | any = null;
-    //state: 'none' | 'walk' | 'rotation' = 'none';
-    axesX: number = 0;
-    axesZ: number = 0;
-    isWalking: boolean = false;
-    isRotating: boolean = false;
-    speed: number = 0.025;
 }
 
 MovementFPSComponent.schema = {
     player: { type: Types.Ref, default: null },
-    //state: { type: Types.String, default: 'none' },
-    axesX: { type: Types.Number, default: 0 },
-    axesZ: { type: Types.Number, default: 0 },
-    isWalking: {type: Types.Boolean, default: false},
-    isRotating: {type: Types.Boolean, default: false},
-    speed: { type: Types.Number, default: 0.25 }
 };
